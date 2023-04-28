da82a5094e5d774945c9702eaf4cfa35b1c0a07c
medetbek_mominov <moskit544@gmail.com>
2023-04-28 23:56:55 +0200

    added public path in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
   transpileDependencies: true,
   chainWebpack: config => {
     const imgRule = config.module.rule('images')
